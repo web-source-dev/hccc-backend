6069621229dbac4cccda4883155065a55c6057f8
web-source-dev <muhammadnouman72321@gmail.com>
2025-07-19 04:57:05 +0500

    Adjusted

--- models/User.js
+++ models/User.js
@@ -4,17 +4,9 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   firstname: {
     type: String,
-    required: [true, 'First name is required'],
-    trim: true,
-    minlength: [2, 'First name must be at least 2 characters long'],
-    maxlength: [50, 'First name cannot exceed 50 characters']
   },
   lastname: {
     type: String,
-    required: [true, 'Last name is required'],
-    trim: true,
-    minlength: [2, 'Last name must be at least 2 characters long'],
-    maxlength: [50, 'Last name cannot exceed 50 characters']
   },
   email: {
     type: String,
